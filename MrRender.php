954786c5aba5ffe01019c16317e22c51d5a427e7
phaziz <phaziz@gmail.com>
2021-06-02 20:07:05 +0200

    MrRender 5.0

--- MrRender.php
+++ MrRender.php
@@ -1,24 +1,24 @@
 <?php
 
-/*
-***************************************************************************
+    /*
+    ***************************************************************************
 
-	DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-	Version 5, 2021
-	Copyright (C) 2021 Christian Becher | phaziz.com <phaziz@gmail.com>
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+        Version 5, 2021
+        Copyright (C) 2021 Christian Becher | phaziz.com <phaziz@gmail.com>
 
-	Everyone is permitted to copy and distribute verbatim or modified
-	copies of this license document, and changing it is allowed as long
-	as the name is changed.
+        Everyone is permitted to copy and distribute verbatim or modified
+        copies of this license document, and changing it is allowed as long
+        as the name is changed.
 
-	DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-	TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-	0. YOU JUST DO WHAT THE FUCK YOU WANT TO!
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+        TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+        0. YOU JUST DO WHAT THE FUCK YOU WANT TO!
 
-	+++ Visit https://github.com/phaziz +++
+        +++ Visit https://github.com/phaziz +++
 
-***************************************************************************
-*/
+    ***************************************************************************
+    */
 
 namespace MrRender;
 
