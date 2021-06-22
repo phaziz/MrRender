9b20cccc3183c5635a7812260c00486016db1813
phaziz <phaziz@gmail.com>
2021-06-22 22:00:18 +0200

    Lottozahlen Tipp Plugin

--- plugins/startseitenContent.php
+++ plugins/startseitenContent.php
@@ -22,5 +22,15 @@
 
     function startseitenContent()
     {
-        return '+ + + This is a startseitenContentPlugin output + + +';
+        $lottozahlen = range(1, 49);
+        $zusatzzahlen = range(0 , 9);
+        $tipp = '';
+
+        for($i = 0; $i < 6; $i++){
+            $tipp .= $lottozahlen[rand(1, 49)] . ' - ';
+        }
+
+        $zusatzzahl = '<strong>' . $zusatzzahlen[rand(0, 9)] . '</strong>';
+
+        return '+ + + Lottozahlen-Tipp: <i>' . $tipp . '</i>' . $zusatzzahl . ' (PluginOutput) + + +';
     }
