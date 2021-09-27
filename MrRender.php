554f552935dc21f5e84f89e5ae9a9d799c770da4
Christian Becher <phaziz@gmail.com>
2021-09-27 22:45:26 +0200

    License

--- MrRender.php
+++ MrRender.php
@@ -1,20 +1,22 @@
 <?php
 
-    /*
-        DO what THE FUCK YOU WANT TO PUBLIC LICENSE
-        Version 5, 2021
-        Copyright (C) 2021 Christian Becher | phaziz.com <phaziz@gmail.com>
+	/*
 
-        Everyone is permitted to copy and distribute verbatim or modified
-        copies of this license document, and changing it is allowed as long
-        as the name is changed.
+		BSD Zero Clause License
+		
+		Copyright (C) 2021 by phaziz <phaziz@gmail.com>
+		
+		Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
 
-        DO what THE FUCK YOU WANT TO PUBLIC LICENSE
-        TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-        0. YOU JUST DO what THE FUCK YOU WANT TO!
+		THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
+		WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, 
+		OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF 
+		CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+		
+		- YOU JUST DO WHAT THE FUCK YOU WANT TO! - 
 
-        +++ Visit https://github.com/phaziz/MrRender/tree/master +++
-    */
+		+++ Visit https://github.com/phaziz/MrRender +++
+	*/
 
     namespace MrRender;
 
--- README.md
+++ README.md
@@ -33,3 +33,16 @@ For example, on your localhost: got to the htdocs-directory, copy the downloaded
 
 You can also use Composer...
 
+License:
+
+BSD Zero Clause License
+
+Copyright (C) 2021 by phaziz <phaziz@gmail.com>
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, 
+OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF 
+CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
